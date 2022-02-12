4999ddd69a78fba185814358876717878ab7ecde
celian <celian.riboulet@gmail.com>
2022-02-12 15:00:00 +0100

    Fix mood

--- src/components/TeamMood.jsx
+++ src/components/TeamMood.jsx
@@ -1,8 +1,8 @@
-import { useCallback, useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 import '../stylesheets/mood.css';
 import { useGlobal } from 'reactn';
-import { collection, doc, setDoc, getDocs } from "firebase/firestore";
+import { collection, doc, setDoc, getDoc } from "firebase/firestore";
 
 import { toast } from 'react-toastify';
 
@@ -18,78 +18,73 @@ const membersDisplayName = {
 
 const moods = ['⌛', '😔', '😡', '😬', '😐', '🙂', '😀'];
 
-const getMoodId = (name, startDate) => {
-  const date = new Date(startDate?.toLocaleDateString('FR'));
-  if (Number.isNaN(date.getTime()))
-    return undefined;
-  return name + '|' + date.getTime();
-}
-
 function TeamMood(props) {
 
   const { weekStart, weekEnd } = props;
   const [db] = useGlobal('firebase');
 
-  const [weekMoods, setWeekMoods] = useState([])
+  const [weekMoods, setWeekMoods] = useState({})
 
-  const moodUpdated = (index, value) => {
-    console.log(index, value);
-    setWeekMoods(old => {
-      old[index] = value
-      return old;
-    });
+  const weekId = useMemo(() => weekStart?.toLocaleDateString('FR').replaceAll('/', '-') ?? null, [weekStart]);
 
-    // const cardsRef = collection(db, 'moods');
-    // const moodId = getMoodId(index);
-    // if (moodId == undefined)
-    //   return;
-    // setDoc(doc(cardsRef, moodId), {
-    //   name: members[index],
-    //   mood: value,
-    //   moodId
-    // });
-    // setWeekMoods(old => [...old, {
-    //   name: members[index],
-    //   mood: value,
-    //   moodId
-    // }])
-    // toast.success('Humeur mise à jour !');
-  };
+  const updateMemberMood = (member, value) => setWeekMoods(old => {
+    const newData = { ...old };
+    newData[member] = value
+
+    const cardsRef = collection(db, 'moods');
+    setDoc(doc(cardsRef, weekId), newData);
+
+    toast.success('Humeur mise à jour !');
+    return newData;
+  });
 
   useEffect(() => {
     getThisWeekMoods();
-  }, [db, weekStart]);
+  }, [db, weekId]);
 
   const getThisWeekMoods = async () => {
-    if (db == undefined)
+    if (db == undefined || weekStart == undefined)
       return;
-    const result = await getDocs(collection(db, 'moods'))
-    const moodsTmp = [...new Array(members.length)].map(() => undefined);
-    result.forEach((r, i) => {
-      moodsTmp[i] = r.data;
-    });
-    console.log(moodsTmp);
-    setWeekMoods(moodsTmp);
+
+    console.log('REFETCH FOR', weekId);
+    const docRef = collection(db, 'moods')
+    const moodsResult = await (await getDoc(doc(docRef, weekId))).data();
+    if (moodsResult != undefined) {
+      setWeekMoods(moodsResult);
+      return;
+    }
+
+    // Contruct this week object has it does not exists
+    const moodsArray = [...new Array(members.length)].map(() => null);
+    const obj = {};
+    moodsArray.forEach((m, index) => {
+      obj[members[index]] = m;
+    })
+    setWeekMoods(obj);
   }
 
-  const borderStyle = (mood) => {
-    if (mood === '⌛' || mood == undefined)
-      return { borderColor: '#d45950' };
-    return {};
-  }
+  useEffect(() => {
+    console.log(weekMoods);
+  }, [weekMoods]);
 
   return (
     <div className="team-mood-container">
-      {weekMoods.map((moodObj, index) => (
-        <div style={borderStyle(moodObj?.mood)} key={index} className='mood-container'>{moodObj?.member}
+      {Object.entries(weekMoods).map(([member, mood], index) => (
+        <div
+          style={(mood === '⌛' || mood == undefined) ? { borderColor: '#d45950' } : {}}
+          key={index}
+          className='mood-container'
+        >
+          {membersDisplayName[member]}
           <select
             option={moods}
-            onChange={(value) => moodUpdated(index, value.target.value)}
-            defaultValue={moodObj?.mood}
+            onChange={(value) => updateMemberMood(member, value.target.value)}
+            key={mood} // Force refresh select https://github.com/ant-design/ant-design/issues/4347
+            defaultValue={mood}
           >
-            {moods?.map((mood, index) => (
-              <option key={index} value={mood}>
-                {mood}
+            {moods?.map((m, index) => (
+              <option key={index} value={m}>
+                {m}
               </option>
             ))}
           </select>
